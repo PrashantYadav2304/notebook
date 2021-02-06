61a3fce1442f200d8b382d74d427c0c99a91b342
PrashantYadav2304 <2000prashant23@gmail.com>
2021-02-06 20:34:35 +0530

    firebase added

--- src/main.js
+++ src/main.js
@@ -4,6 +4,20 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAhPhujDM6U0mXABkyfBkpnHpLQC3IvZvQ",
+  authDomain: "smartnotebook-in.firebaseapp.com",
+  databaseURL: "https://smartnotebook-in.firebaseio.com",
+  projectId: "smartnotebook-in",
+  storageBucket: "smartnotebook-in.appspot.com",
+  messagingSenderId: "298959463669",
+  appId: "1:298959463669:web:9bd30f5e5dfd17369ad1bc",
+  measurementId: "G-2V1ES0191W"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
